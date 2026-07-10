e30897f4a62178064ee86e2d2e3a5749cd7028e6
kxuak <kauad4867@gmail.com>
2026-07-10 10:53:34 -0300

    terceiro

--- src/layouts/LadingPage/index.tsx
+++ src/layouts/LadingPage/index.tsx
@@ -1,7 +1,13 @@
+import { NavLink } from "react-router-dom"
+import "./index.css"
 
 const LadingPage = () => {
   return (
-    <div>LadingPage</div>
+    <div>
+      <div className="sim">LadingPage</div>
+
+      <NavLink className='paginas' to="/home"> <img src="/home-2.svg"/> Home</NavLink>
+    </div>
   )
 }
 
